6cd406f33f8792cc4190d93fb393fa2ed6c23c3b
Ethan Brimhall <ethannbrimhall@gmail.com>
2019-02-13 15:08:40 -0500

    Bar graph shows after voting has taken place to show results

---
+++ public/js/pollGraph.js
@@ -0,0 +1,22 @@
+var option1 = document.getElementById('number-of-votes1').innerHTML;
+var option2 = document.getElementById('number-of-votes2').innerHTML;
+
+option1 = parseInt(option1);
+option2 = parseInt(option2);
+
+var option1Value = (option1 / (option1 + option2)) * 500;
+var option2Value = (option2 / (option1 + option2)) * 500;
+
+console.log(option1Value);
+console.log(option2Value);
+
+if(option1Value == 0){
+  option1Value = 10;
+}
+
+if(option2Value == 0){
+  option2Value = 10;
+}
+
+document.getElementById('option1Graph').style.width = option1Value + "px";
+document.getElementById('option2Graph').style.width = option2Value + "px";
